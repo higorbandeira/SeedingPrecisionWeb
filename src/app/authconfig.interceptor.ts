2362805f45f69de659c8e828c47735002b8e0a5a
higor.bandeira <higor.bandeira@oransys.net>
2020-10-05 21:02:00 -0300

    wip 1005

--- src/app/authconfig.interceptor.ts
+++ src/app/authconfig.interceptor.ts
@@ -5,7 +5,7 @@ import { AuthService } from "./auth.service";
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) { }
+    constructor() { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         const authToken = localStorage.getItem('access_token');;
