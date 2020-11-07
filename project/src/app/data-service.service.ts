4ebace1716a9a1c13fe62a392e14fe3e6fa078df
higor.bandeira <higor.bandeira@oransys.net>
2020-11-07 19:01:24 -0300

    Nova URL

--- project/src/app/data-service.service.ts
+++ project/src/app/data-service.service.ts
@@ -21,8 +21,8 @@ export class DataServiceService {
   public Agrupamento: string;
   public periods:string[];
 
-  //URL: string = "http://seedingprecisionapi.azurewebsites.net";
-  URL: string = "https://localhost:5001/";
+  URL: string = "http://seedingapi.azurewebsites.net";
+  //URL: string = "https://localhost:5001/";
   URLeX: string = "https://cors-anywhere.herokuapp.com/http://wttr.in/";
 
   async loadDataStatusAtual(){
